5c239927b154b882c4c4db1b3f4f15fe87b565ac
Code Nova <netdot1234@gmail.com>
2026-05-17 20:31:42 +0100

    feat: remove unused JAMB subjects from the import configuration

--- import-jamb-questions.js
+++ import-jamb-questions.js
@@ -12,9 +12,6 @@ const DEFAULT_BUCKET = 'jamb-question-images';
 const REQUEST_TIMEOUT_MS = 30000;
 
 const JAMB_SUBJECTS = [
-  { slug: 'english-language', name: 'Use of English' },
-  { slug: 'civic-education', name: 'Civic Education' },
-  { slug: 'literature-in-english', name: 'Literature-in-English' },
   { slug: 'christian-religious-knowledge-crk', name: 'Christian Religious Studies (CRS)' },
   { slug: 'islamic-religious-knowledge-irk', name: 'Islamic Religious Studies (IRS)' },
   { slug: 'history', name: 'History' },
